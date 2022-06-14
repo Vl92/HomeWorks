b7ec84bb5a50dc536c64d7c59b25af1eab816834
Владислав Матвеев <ultrotech@gmail.com>
2022-06-15 00:46:24 +0300

    Add task HW04.Combiner

---
+++ Lesson04/HW04.Combiner/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HW04.Combiner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите вашу фамилию: ");
+            string surname = Console.ReadLine();
+
+            Console.Write("Здравствуйте, " + name +" " + surname +  ", сегодня хороший день");
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
