5a8cdfbadaf4a6ffdc6d667a20c1a2e08c32281c
Владислав Матвеев <ultrotech@gmail.com>
2022-06-22 13:58:35 +0300

    Add HW05.IntuitionTraining

---
+++ Lesson05/HW05.IntuitionTraining/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace HW05.IntuitionTraining
+{
+    internal class Program
+    {       
+        public static object Guess { get; private set; }
+
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Random random = new Random();
+                int returnValue = random.Next(1, 5);
+                int Guess = 0;
+                Console.WriteLine("Я загадал число от 1 до 5. Сможешь угадать?");
+
+
+
+                Guess = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Вы загадали число " + Guess);
+
+
+                if (Guess != returnValue)
+                {
+                    Console.WriteLine("К сожалению вы не угадали. Попробуйте еще раз");
+                }
+                else if (Guess == returnValue)
+                {
+                    Console.WriteLine("Красавчик, правильный ответ " + returnValue);
+                }
+                Console.WriteLine("Хотите продолжить?");
+
+
+            } 
+
+            Console.ReadLine();
+
+        }
+    }
+}
