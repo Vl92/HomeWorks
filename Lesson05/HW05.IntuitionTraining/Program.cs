753151a6e8a00a96797373d0dcb258675a0134e0
Владислав Матвеев <ultrotech@gmail.com>
2022-06-25 22:10:32 +0300

    Bug correction

--- Lesson05/HW05.IntuitionTraining/Program.cs
+++ Lesson05/HW05.IntuitionTraining/Program.cs
@@ -13,28 +13,22 @@ namespace HW05.IntuitionTraining
             {
                 Random random = new Random();
                 int returnValue = random.Next(1, 5);
-                int Guess = 0;
+                int userImput = 0;
                 Console.WriteLine("Я загадал число от 1 до 5. Сможешь угадать?");
 
+                userImput = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Вы загадали число " + userImput);
 
-
-                Guess = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Вы загадали число " + Guess);
-
-
-                if (Guess != returnValue)
+                if (userImput != returnValue)
                 {
                     Console.WriteLine("К сожалению вы не угадали. Попробуйте еще раз");
                 }
-                else if (Guess == returnValue)
+                else if (userImput == returnValue)
                 {
                     Console.WriteLine("Красавчик, правильный ответ " + returnValue);
                 }
                 Console.WriteLine("Хотите продолжить?");
-
-
             } 
-
             Console.ReadLine();
 
         }
--- Lesson05/HW05.SchoolControls/Program.cs
+++ Lesson05/HW05.SchoolControls/Program.cs
@@ -5,28 +5,26 @@ namespace HW05.SchoolControls
     internal class Program
     {
         static void Main(string[] args)
-        {
-            
+        {            
             while (true)
             {
                 Console.WriteLine("Введите номер контрольного теста");
-                int numtest = 0;
-                numtest = Convert.ToInt32(Console.ReadLine());
+                int testNumber = 0;
+                testNumber = Convert.ToInt32(Console.ReadLine());
 
-
-                if ((numtest >= 1) && (numtest <= 15))
+                if ((testNumber >= 1) && (testNumber <= 15))
                 {
                     Console.WriteLine("Тест который вы ввели относится к первой четверти");
                 }
-                else if ((numtest >= 16) && (numtest <= 30))
+                else if ((testNumber >= 16) && (testNumber <= 30))
                 {
                     Console.WriteLine("Тест который вы ввели относится ко второй четверти");
                 }
-                else if ((numtest >= 31) && (numtest <= 45))
+                else if ((testNumber >= 31) && (testNumber <= 45))
                 {
                     Console.WriteLine("Тест который вы ввели относится к третьей четверти");
                 }
-                else if ((numtest >= 46) && (numtest <= 60))
+                else if ((testNumber >= 46) && (testNumber <= 60))
                 {
                     Console.WriteLine("Тест который вы ввели относится к четвертой четверти");
                 }
