e95b97e54adff9845fd21bb0bd760d7d52e127b7
Владислав Матвеев <ultrotech@gmail.com>
2022-07-02 23:29:30 +0300

    Add HW06.TheTriangleBuilder

---
+++ Lesson06/HW06.TheTriangleBuilder/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HW06.TheTriangleBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var userInput = Convert.ToInt32(Console.ReadLine());
+            var counterOne = userInput;
+
+            var displayValue = 0;
+
+            for (int i = userInput; i >=0; i--)
+            {
+                var counterTwo  = userInput - counterOne;
+                for (int k = 0; k <= counterTwo; k++) 
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j <= counterOne; j++)
+                {
+                    Console.Write(displayValue);
+                    Console.Write(" ");
+                }
+                displayValue++;
+                if(displayValue == 10)
+                {
+                    displayValue = 0;
+                }
+                Console.WriteLine();
+                counterOne--;
+            }
+            Console.ReadKey();
+                
+        }
+    }
+}
