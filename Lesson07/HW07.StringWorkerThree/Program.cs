41698180682da23bbc1ac81c2823fb0080593fe7
Владислав Матвеев <ultrotech@gmail.com>
2022-07-04 01:50:09 +0300

    Add HW07.StringWorkerThree

---
+++ Lesson07/HW07.StringWorkerThree/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace HW07.StringWorkerThree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {          
+            string initialВata = "gdfgdf234dg54gf+23oP42";
+            Console.WriteLine("Изначальное значение: " + initialВata);
+            string[] signSeparation = initialВata.Split('+','-','*','/');
+
+            foreach (var word in signSeparation)
+            { }                      
+            
+            int value;
+            int.TryParse(string.Join("", signSeparation[0].Where(c => char.IsDigit(c))), out value);
+            Console.WriteLine("Первое значение: " + value);
+
+            int valueTwo;
+            int.TryParse(string.Join("", signSeparation[1].Where(c => char.IsDigit(c))), out valueTwo);
+            Console.WriteLine("Второе значение: "+valueTwo);
+
+            int sum = value + valueTwo;
+            Console.WriteLine(sum);
+       
+            Console.ReadKey();
+        }
+    }
+}
