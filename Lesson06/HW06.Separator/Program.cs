323e05be309000fe49be61231bffda0c540a84f4
Владислав Матвеев <ultrotech@gmail.com>
2022-07-05 18:50:50 +0300

    Add HW06.Separator,HW06.TheTriangleBuilder,HW06.ThirdArrayCreator

---
+++ Lesson06/HW06.Separator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HW06.Separator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userImput = "Я помню чудное мгновенье:; Передо мной явилась ты,;" +
+                               "Как мимолетное виденье, ; Как гений чистой красоты.";
+            string[] splitedArray = userImput.Split(';');
+            string poem = "";
+            for (int i = 0; i < splitedArray.Length; i++)
+            {
+                poem += (splitedArray[i] + "\n").Trim(' ').Replace('о', 'а');
+            }
+            Console.WriteLine(poem);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson06/HW06.TheTriangleBuilder/HW06.TheTriangleBuilder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HW06.TheTriangleBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            Console.WriteLine("Enter one or more lines of text (press CTRL+Z to exit):");
+            Console.WriteLine();
+            do
+            {
+                Console.Write("   ");
+                line = Console.ReadLine();
+                if (line != null)
+                    Console.WriteLine("      " + line);
+            } while (line != null);
+        }
+    }
+}
